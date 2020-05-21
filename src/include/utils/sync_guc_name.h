55f67ce59ef0a8420495c0df8d67f418046f098f
Abhijit Subramanya <asubramanya@pivotal.io>
2020-05-21 15:27:39 -0700

    Address PR feedback

--- src/include/utils/sync_guc_name.h
+++ src/include/utils/sync_guc_name.h
@@ -94,7 +94,6 @@
 		"optimizer_partition_selection_log",
 		"optimizer_plan_id",
 		"optimizer_push_group_by_below_setop_threshold",
-		"optimizer_eageragg_threshold",
 		"optimizer_samples_number",
 		"planner_work_mem",
 		"pljava_classpath",
